681fe90dd5060de69211bb2aaede90067a233cae
supenser <fenders390@gmail.com>
2023-12-07 20:49:08 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,14 @@ class User < ApplicationRecord
          VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
          VALID_PASSWORD_REGEX = /\A(?=.*[a-z])(?=.*\d)[a-z\d]{6,}+\z/
 
-        validates :nickname, presence: true
-        validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX, message: "を○○@○○の形式で入力して下さい" }, length: { maximum: 255 }
+        validates :nickname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "が空欄のため入力してください"}
+        validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX, message: "を○○@○○.○○の形式で入力して下さい" }, length: { maximum: 255 }
         validates :password, presence: true, confirmation: true, format: { with: VALID_PASSWORD_REGEX, message: "を半角英数字6文字以上で入力して下さい" }, length: { maximum: 129 }
-        with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'に全角文字を使用してください' } do
+        with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "に全角文字を使用してください" } do
           validates :last_name
           validates :first_name
         end
-        with_options presence: true,format: {with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/, message: "全角カタカナのみで入力して下さい"} do
+        with_options presence: true,format: {with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/, message: "に全角カタカナのみで入力して下さい"} do
           validates :kana_last_name
           validates :kana_first_name
         end
