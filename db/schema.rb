3e88920ff5e15a397cca5258b6ae65fdaa186407
supenser <fenders390@gmail.com>
2023-12-11 16:40:56 +0900

    itemモデル設定・アソシエーション設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_05_013716) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_11_005723) do
+  create_table "items", charset: "utf8", force: :cascade do |t|
+    t.string "item", null: false
+    t.text "item_text", null: false
+    t.integer "category_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "delivery_cost_id", null: false
+    t.integer "area_id", null: false
+    t.integer "shipping_alate_id", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -29,4 +44,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_05_013716) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
