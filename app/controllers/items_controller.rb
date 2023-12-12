3c3142071a4ec0ad3a29bea7acf5cf5ec1e506be
supenser <fenders390@gmail.com>
2023-12-12 11:41:50 +0900

    ActiveStorage実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,21 @@
 class ItemsController < ApplicationController
   def index
+    @items = item.all
   end
+
+  def new
+    @item = item.new
+  end
+
+  def create
+    @item = item.new(item_params)
+
+  end
+
+private
+
+  def item_params
+    params.require(:item).permit(:item, :item_text, :category_id, :condition_id, :delivery_cost_id, :area_id, :shipping_alate_id, :price, :image).merge( user_id: current_user.id)
+  end
+
 end
