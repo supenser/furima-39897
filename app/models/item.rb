89010c9540ee48cc1a87753bf185230d2d9262bc
supenser <fenders390@gmail.com>
2023-12-14 21:36:43 +0900

    Activeハッシュ実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,18 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  # belongs_to :condition
+  # belongs_to :delivery_cost
+  # belongs_to :area
+  # belongs_to :shipping_date
 
+  validates :item, :item_text, :category, :condition, :delivery_cost, :area, :shipping_date, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_cost_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_date_id, numericality: { other_than: 1, message: "can't be blank" }
 
 end
