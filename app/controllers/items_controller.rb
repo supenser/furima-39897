8344dbcd630042edddaef052a0708a9e38b3d7f7
supenser <fenders390@gmail.com>
2023-12-24 03:32:34 +0900

    エラー発生時のメッセージ表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,9 +28,9 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    if item.update(item_params)
-    redirect_to root_path
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
     else
       render :edit, status: :unprocessable_entity
     end
